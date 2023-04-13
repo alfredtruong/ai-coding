ea5caf11e92178bf36c396f953f0005d9b814031
akyt2 <alfred.truong@gmail.com>
2023-04-13 18:40:06 +0800

    move

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "ETL/ETL.h"
 #include "LinearRegression/LinearRegression.h"
-#include "LogisticRegression.h"
+#include "LogisticRegression/LogisticRegression.h"
 
 /*
-https://www.youtube.com/watch?v=jKtbNvCT8Dc
+https://www.youtube.com/watch?v=jKtbNvCT8Dc // linear regression
+https://www.youtube.com/watch?v=4ICxrzWIi3I // logistic regression
 */
 
 using namespace std;
@@ -26,24 +27,24 @@ int LinearRegression_main()
 
   // test dataframe
   Eigen::MatrixXd dataMat = etl.CSVtoEigen(dataset, rows, cols);
-  //std::cout << dataMat << std::endl;
+  std::cout << dataMat << std::endl;
   //std::cout << dataMat.rows() << " " << dataMat.cols() << std::endl;
 
   // test normalization
   Eigen::MatrixXd norm = etl.Normalize(dataMat);
-  //std::cout << norm << std::endl;
+  std::cout << norm << std::endl;
   //std::cout << norm.rows() << " " << norm.cols() << std::endl;
 
   // test TrainTestSplit
   Eigen::MatrixXd X_train, y_train, X_test, y_test;
   std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> split_data = etl.TrainTestSplit(norm, 0.8);
-  std:tie(X_train, y_train, X_test, y_test) = split_data;
-  /*
+  std::tie(X_train, y_train, X_test, y_test) = split_data;
+
   std::cout << X_train.rows() << " " << X_train.cols() << std::endl;
   std::cout << y_train.rows() << " " << y_train.cols() << std::endl;
   std::cout << X_test.rows() << " " << X_test.cols() << std::endl;
   std::cout << y_test.rows() << " " << y_test.cols() << std::endl;
-  */
+
 
   // test linear regression
   Eigen::VectorXd vec_train = Eigen::VectorXd::Ones(X_train.rows());
@@ -64,13 +65,11 @@ int LinearRegression_main()
   std::tuple<Eigen::VectorXd, std::vector<float>> gd = lr.GradientDescent(X_train, y_train, theta, alpha, iters);
   std::tie(thetaOut, cost) = gd;
 
-  /*
   std::cout << "theta = " << thetaOut << std::endl;
   for (auto v : cost)
   {
     std::cout << v << std::endl;
   }
-  */
 
   // export to file
   etl.EigenToFile(thetaOut,"LinearRegression/thetaOut.txt");
@@ -111,6 +110,7 @@ int LogisticRegression_main()
 
   std::cout << rows << std::endl;
   std::cout << cols << std::endl;
+
   return 0;
 }
 
